98ffc885b6b8747f303d440bb634b5ae1c7b11cd
Oak <sivakornchong@gmail.com>
2022-08-19 16:15:31 +0800

    allow nearest mrt calc

--- get_features.py
+++ get_features.py
@@ -1,12 +1,59 @@
-import pandas as pd
 import json
+import requests
+from misc_fn import nearest_mrt
 
-tuple_mrt= {}
+count, fail_count = 0, 0 
+dst = open('data_w_coord.json', 'w')
+
+###This is to create MRT names and MRT locations
+mrt_name = []
+mrt_loc = []
 with open('mrt_list.json', 'r') as file:
     for line in file:
         item = json.loads(line)
-        print(item['MRT'])
-        print(tuple(item['location']))
+        mrt_name.append(item['MRT'])
+        loc = tuple([float(i) for i in item['location']])
+        mrt_loc.append(loc)
 
-print(tuple_mrt)
-    
+
+with open('data_source.json', 'r') as file:
+    for line in file:
+        try:
+            item = json.loads(line)
+            search_term = item['block']+" "+item['street_name']
+            # print(search_term)
+            count += 1
+            #Query for the lat/ long
+            query_string='https://developers.onemap.sg/commonapi/search?searchVal={}&returnGeom=Y&getAddrDetails=Y&pageNum=1'.format(search_term)
+            resp = requests.get(query_string)
+            data = json.loads(resp.content)
+            chosen_result = data['results'][0]
+            #Calculate the nearest MRT
+            distance_km, nearest_mr = nearest_mrt(chosen_result['LATITUDE'], chosen_result['LONGITUDE'], mrt_name, mrt_loc)
+            # print(data)
+            labelled_data = {
+                'Location': search_term,
+                'Postal': chosen_result['POSTAL'],
+                'latitude': chosen_result['LATITUDE'],
+                'longitude': chosen_result['LONGITUDE'],
+                'distance_mrt': distance_km,
+                'nearest_mrt': nearest_mr,
+                'resale_info': item
+            }
+            json_data = json.dumps(labelled_data)
+            dst.write(json_data+"\n")
+
+        except Exception as e:
+            print('error as', e)
+            fail_count += 1
+        
+        if count % 100 == 0:
+            print('Iterating successful to count:', count)
+            print('Current fail count:', fail_count)
+            print("sample")
+            print(distance_km)
+            print(nearest_mr)
+
+print('='*100)
+print('successful house search, location added:', count)
+print('unsuccessful house search, location added:', fail_count)
--- misc_fn.py
+++ misc_fn.py
@@ -1,4 +1,6 @@
 from dateutil.parser import parse
+import geopy.distance
+import json
 
 def is_date(string, fuzzy=False):
     """
@@ -13,3 +15,36 @@ def is_date(string, fuzzy=False):
 
     except ValueError:
         return False
+
+def distance_to_mrt(lat, long, location):
+    coord_mrt = tuple(location)
+    coord_house = (lat, long)
+    distance_km = geopy.distance.distance(coord_mrt, coord_house).km
+    #print(distance_km)
+    return distance_km
+
+def nearest_mrt(lat, long, mrt_name, mrt_loc):
+    count = 0
+    distance_km = 100
+    for mrt in mrt_name:
+        distance_cal = distance_to_mrt(lat, long, mrt_loc[count])
+        if distance_cal < distance_km:
+            nearest_mr = mrt
+            distance_km = distance_cal
+        count += 1
+    return distance_km, nearest_mr
+
+# if __name__ == "__main__":
+    
+#     mrt_name = []
+#     mrt_loc = []
+#     with open('mrt_list.json', 'r') as file:
+#         for line in file:
+#             item = json.loads(line)
+#             mrt_name.append(item['MRT'])
+#             loc = tuple([float(i) for i in item['location']])
+#             mrt_loc.append(loc)
+
+#     distance_km, nearest_mr = nearest_mrt(1.34849544899052, 103.875566867625, mrt_name, mrt_loc)
+    # print(distance_km)
+    # print(nearest_mr)
