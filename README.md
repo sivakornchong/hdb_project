fe6f9de8a0151a6a7c43ac53ceadabb4eb5fb5cd
Oak <sivakornchong@gmail.com>
2022-11-15 17:48:42 +0800

    create notebook

--- README.md
+++ README.md
@@ -6,7 +6,20 @@ Intent:
 
 
 Code:
-Block 1 - API
+Block 1 - API, get features
+    1. connect_api.py - obtain information from the api on all the resale flats. Write to data_source.json
+    1a. create_mrt_list.py - create mrt list from wikipedia table in MRT_from_wiki.csv. Write to mrt_list.json.
+    2. get_features.py - Add further information for each flat. Data_source.json translated to data_features.json
+
+    
+    
+
 Block 2 - Backend calculation
+    3a. feature_eng_hot.py   This includes model training with OneHot. 
+    Three algorithms are tested.
+    3b. feature_eng_ord.py  Backup file with just decision tree regressor
+    
+
 Block 3 - Visualization
+
 Block 4 - Predictive analysis: price of new launch at MOP
--- feature_eng_hot.py
+++ feature_eng_hot.py
@@ -10,6 +10,9 @@ columns = ['distance_mrt','age_transation','lease_commence',\
 
 columns_cat = ['town','flat_num']
 
+print('Length of data is:', dataset.shape[0])
+print(dataset.head())
+
 dataset_clean = dataset[columns].copy()
 dataset_cat = dataset[columns_cat].copy()
 
@@ -51,17 +54,33 @@ from sklearn.tree import DecisionTreeRegressor
 # measuring RMSE score
 from sklearn.metrics import mean_squared_error
 
+print('_'*100)
+print('Identifying the most important parameters')
+
+dt = DecisionTreeRegressor(max_depth=30,random_state=27)
+dt.fit(X_train,y_train)
+pred = dt.predict(X_test)
+rmse = np.sqrt(mean_squared_error(y_test,pred))
+# print('rmse for decision tree, OneHot, normalized:', rmse)
+
+feature_importances__ = pd.DataFrame(dt.feature_importances_,
+                                   index = X.columns,
+                                    columns=['importance']).sort_values('importance', ascending=False)
+
 # Decision tree
 x_axis= []
 y_axis = []
-for i in range(25,35):
+print('_'*100)
+print('testing out decision tree, OneHot, normalized')
+for i in range(20,35):
     dt = DecisionTreeRegressor(max_depth=i,random_state=27)
     dt.fit(X_train,y_train)
     pred = dt.predict(X_test)
     rmse = np.sqrt(mean_squared_error(y_test,pred))
-    #print('rmse for decision tree, OneHot, normalized:', rmse)
+    # print('rmse for decision tree, OneHot, normalized:', rmse)
     x_axis.append(i)
     y_axis.append(rmse)
+
 
 min_rmse = min(y_axis)
 min_depth = x_axis[y_axis.index(min_rmse)]
@@ -73,8 +92,10 @@ print('rmse for decision tree, OneHot, normalized:', min_rmse, 'at depth =', min
 #Trying out without normalization. 
 x_axis = []
 y_axis = []
+print('_'*100)
+print('testing out decision tree, OneHot, not normalized')
 X_train_raw, X_test_raw, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-for i in range(25,35):
+for i in range(20,35):
     dt = DecisionTreeRegressor(max_depth=30,random_state=27)
     dt.fit(X_train_raw,y_train)
     pred = dt.predict(X_test_raw)
@@ -89,6 +110,8 @@ print('rmse for decision tree, OneHot, not normalized:', min_rmse, 'at depth =',
 
 
 #Trying out knn algorihtm
+print('_'*100)
+print('testing out KNN, OneHot, normalized')
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error
 knn = KNeighborsRegressor(algorithm='auto')
@@ -99,6 +122,6 @@ rmse = mse ** (1/2)
 print('rmse for KNN, OneHot, normalized:', rmse)
 
 ###Performance compiled: normalized OneHot is the best. 
-#rmse for decision tree, OneHot, normalized: 36201.34658490505 at depth = 30
+#rmse for decision tree, OneHot, normalized: 35825.029340292735 at depth = 30
 #rmse for decision tree, OneHot, not normalized: 35896.29191019776 at depth = 25
-# rmse for KNN, OneHot, normalized: 36065.8015334932
+#rmse for KNN, OneHot, normalized: 36065.8015334932
