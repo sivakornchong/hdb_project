34d33f527a5023238afe804b9ae88992faa85379
Sivakorn Chong <sivakornchong@gmail.com>
2023-11-12 12:55:50 -0800

    Add multiprocessing

--- get_features_mp.py
+++ get_features_mp.py
@@ -1,48 +1,48 @@
 import json
 import requests
 from misc_fn import nearest_mrt, numerical
-import time
-from multiprocessing.dummy import Pool as ThreadPool    
-
+from multiprocessing.dummy import Pool as ThreadPool
+from tqdm import tqdm 
 import functools
 
-def generate(mrt_name, mrt_loc, dst, line):
-    item = json.loads(line)
-    search_term = item['block']+" "+item['street_name']
-    # print(search_term)
-    #Query for the lat/ long
-    query_string='https://developers.onemap.sg/commonapi/search?searchVal={}&returnGeom=Y&getAddrDetails=Y&pageNum=1'.format(search_term)
-    resp = requests.get(query_string)
-    data = json.loads(resp.content)
-    chosen_result = data['results'][0]
-    #Calculate the nearest MRT
-    distance_km, nearest_mr = nearest_mrt(chosen_result['LATITUDE'], chosen_result['LONGITUDE'], mrt_name, mrt_loc)
-    town_num, flat_num, area, age, transaction, storey, resale_price_adj = numerical(item)
-    # print(data)
-    labelled_data = {
-        'distance_mrt': distance_km,
-        'town': town_num,
-        'area': area,
-        'flat_num': flat_num,
-        'age_transation': age,
-        'lease_commence': item['lease_commence_date'],
-        'transaction_yr': transaction,
-        'transaction': item['month'],
-        'storey_height': storey,
-        'resale_price': item['resale_price'],
-        'resale_price_adj': resale_price_adj,
-        #find age at translation
-        # 'resale_info': item,
-        'Postal': chosen_result['POSTAL'],
-        'Location': search_term
-    }
-    json_data = json.dumps(labelled_data)
-    dst.write(json_data+"\n")
+def generate(mrt_name, mrt_loc, line):
+    try:  
+        item = json.loads(line)
+        search_term = item['block']+" "+item['street_name']
+        # print(search_term)
+        #Query for the lat/ long
+        query_string='https://developers.onemap.sg/commonapi/search?searchVal={}&returnGeom=Y&getAddrDetails=Y&pageNum=1'.format(search_term)
+        resp = requests.get(query_string)
+        data = json.loads(resp.content)
+        chosen_result = data['results'][0]
+        #Calculate the nearest MRT
+        distance_km, nearest_mr = nearest_mrt(chosen_result['LATITUDE'], chosen_result['LONGITUDE'], mrt_name, mrt_loc)
+        town_num, flat_num, area, age, transaction, storey, resale_price_adj = numerical(item)
+        # print(data)
+        labelled_data = {
+            'distance_mrt': distance_km,
+            'town': town_num,
+            'area': area,
+            'flat_num': flat_num,
+            'age_transation': age,
+            'lease_commence': item['lease_commence_date'],
+            'transaction_yr': transaction,
+            'transaction': item['month'],
+            'storey_height': storey,
+            'resale_price': item['resale_price'],
+            'resale_price_adj': resale_price_adj,
+            #find age at translation
+            # 'resale_info': item,
+            'Postal': chosen_result['POSTAL'],
+            'Location': search_term
+        }
+        data_list.append(labelled_data)
+    except Exception as e:
+        print('error as', e)
+        print('for dataset', line)
+        
 
 if __name__ == "__main__":
-    dst = open('data/data_features.json', 'w')
-    st = time.time()
-
     ###This is to create MRT names and MRT locations
     mrt_name = []
     mrt_loc = []
@@ -53,12 +53,31 @@ if __name__ == "__main__":
             loc = tuple([float(i) for i in item['location']])
             mrt_loc.append(loc)
     
+    #This part aims to transform the inputs into usable features
     print("set up ready for multiprocessing")
+    data_list = []
+    source_address = 'data/data_source.json'
+    total_lines = sum(1 for _ in open(source_address, 'r'))
 
-    partial_generate = functools.partial(generate, mrt_name, mrt_loc, dst)
-    pool = ThreadPool(256)
+    partial_generate = functools.partial(generate, mrt_name, mrt_loc)
+    num_threads = 100
+    print("Total lines for iteration:", total_lines)
 
-    with open('data/data_source_10k.json', 'r') as file:
-        pool.map(partial_generate, file)
-        pool.close()
-        pool.join()
+    with ThreadPool(num_threads) as pool:
+        partial_generate_args = [line for line in open(source_address, 'r')]
+        r = list(tqdm(pool.imap(partial_generate, partial_generate_args), total=total_lines))
+
+    # with open(source_address, 'r') as file:
+    #     pool.map(partial_generate, file)
+    #     pool.close()
+    #     pool.join()
+
+    #Write to a file eventually
+    count = 0
+    dst = open('data/data_features.json', 'w')
+    for item in data_list:
+        json_data = json.dumps(item)
+        dst.write(json_data+"\n")
+        count += 1
+    
+    print("Successfully write out", count)
---
+++ retired/get_features.py
@@ -0,0 +1,138 @@
+import json
+import requests
+from misc_fn import nearest_mrt, numerical
+import time
+from multiprocessing.dummy import Pool as ThreadPool    
+
+import functools
+
+def generate(mrt_name, mrt_loc, dst, line):
+    item = json.loads(line)
+    search_term = item['block']+" "+item['street_name']
+    # print(search_term)
+    #Query for the lat/ long
+    query_string='https://developers.onemap.sg/commonapi/search?searchVal={}&returnGeom=Y&getAddrDetails=Y&pageNum=1'.format(search_term)
+    resp = requests.get(query_string)
+    data = json.loads(resp.content)
+    chosen_result = data['results'][0]
+    #Calculate the nearest MRT
+    distance_km, nearest_mr = nearest_mrt(chosen_result['LATITUDE'], chosen_result['LONGITUDE'], mrt_name, mrt_loc)
+    town_num, flat_num, area, age, transaction, storey, resale_price_adj = numerical(item)
+    # print(data)
+    labelled_data = {
+        'distance_mrt': distance_km,
+        'town': town_num,
+        'area': area,
+        'flat_num': flat_num,
+        'age_transation': age,
+        'lease_commence': item['lease_commence_date'],
+        'transaction_yr': transaction,
+        'transaction': item['month'],
+        'storey_height': storey,
+        'resale_price': item['resale_price'],
+        'resale_price_adj': resale_price_adj,
+        #find age at translation
+        # 'resale_info': item,
+        'Postal': chosen_result['POSTAL'],
+        'Location': search_term
+    }
+    json_data = json.dumps(labelled_data)
+    dst.write(json_data+"\n")
+
+if __name__ == "__main__":
+    dst = open('data/data_features.json', 'w')
+    st = time.time()
+
+    ###This is to create MRT names and MRT locations
+    mrt_name = []
+    mrt_loc = []
+    with open('data/mrt_list.json', 'r') as file:
+        for line in file:
+            item = json.loads(line)
+            mrt_name.append(item['MRT'])
+            loc = tuple([float(i) for i in item['location']])
+            mrt_loc.append(loc)
+    
+    print("set up ready for multiprocessing")
+
+    partial_generate = functools.partial(generate, mrt_name, mrt_loc, dst)
+    pool = ThreadPool(256)
+
+    with open('data/data_source_10k.json', 'r') as file:
+        pool.map(partial_generate, file)
+        pool.close()
+        pool.join()
+
+    # with open('data/data_source.json', 'r') as file:
+    #     for i, line in enumerate(file):
+    #         item = json.loads(line)
+    #         search_term = item['block']+" "+item['street_name']
+    #         # print(search_term)
+    #         #Query for the lat/ long
+    #         query_string='https://developers.onemap.sg/commonapi/search?searchVal={}&returnGeom=Y&getAddrDetails=Y&pageNum=1'.format(search_term)
+    #         resp = requests.get(query_string)
+    #         data = json.loads(resp.content)
+    #         chosen_result = data['results'][0]
+    #         #Calculate the nearest MRT
+    #         distance_km, nearest_mr = nearest_mrt(chosen_result['LATITUDE'], chosen_result['LONGITUDE'], mrt_name, mrt_loc)
+    #         print(distance_km, nearest_mr)
+    #         if i>1:
+    #             break
+
+        
+
+
+
+# with open('data/data_source.json', 'r') as file:
+#     for line in file:
+#         try:
+#             item = json.loads(line)
+#             search_term = item['block']+" "+item['street_name']
+#             # print(search_term)
+#             #Query for the lat/ long
+#             query_string='https://developers.onemap.sg/commonapi/search?searchVal={}&returnGeom=Y&getAddrDetails=Y&pageNum=1'.format(search_term)
+#             resp = requests.get(query_string)
+#             data = json.loads(resp.content)
+#             chosen_result = data['results'][0]
+#             #Calculate the nearest MRT
+#             distance_km, nearest_mr = nearest_mrt(chosen_result['LATITUDE'], chosen_result['LONGITUDE'], mrt_name, mrt_loc)
+#             town_num, flat_num, area, age, transaction, storey, resale_price_adj = numerical(item)
+#             # print(data)
+#             labelled_data = {
+#                 'distance_mrt': distance_km,
+#                 'town': town_num,
+#                 'area': area,
+#                 'flat_num': flat_num,
+#                 'age_transation': age,
+#                 'lease_commence': item['lease_commence_date'],
+#                 'transaction_yr': transaction,
+#                 'transaction': item['month'],
+#                 'storey_height': storey,
+#                 'resale_price': item['resale_price'],
+#                 'resale_price_adj': resale_price_adj,
+#                 #find age at translation
+#                 # 'resale_info': item,
+#                 'Postal': chosen_result['POSTAL'],
+#                 'Location': search_term
+#             }
+#             json_data = json.dumps(labelled_data)
+#             dst.write(json_data+"\n")
+#             count += 1
+
+#             if count % 100 == 0:
+#                 et = time.time()
+#                 time_elapsed = et - st
+#                 print('Iterating successful to count:', count)
+#                 print('Current fail count:', fail_count)
+#                 print('time_elapsed', time_elapsed, 'seconds')
+#                 print("sample")
+#                 print(labelled_data)
+
+#         except Exception as e:
+#             print('error as', e)
+#             fail_count += 1
+        
+
+# print('='*100)
+# print('successful house search, location added:', count)
+# print('unsuccessful house search, location added:', fail_count)
