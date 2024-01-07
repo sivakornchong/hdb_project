b855789f48477df261a366a63d91b2a289fda920
Sivakorn Chong <sivakornchong@gmail.com>
2024-01-07 01:19:58 +0000

    Run from GCP VM

--- boot.sh
+++ boot.sh
@@ -1,12 +1,13 @@
+cd ~/hdb_project
 
-# rm -r data/2024_pipe model
-# mkdir data/2024_pipe
-# mkdir model
+rm -r data/2024_pipe model
+mkdir data/2024_pipe
+mkdir model
 
 ## Run the Python script
-# python src/connect_api.py &&
-# python src/get_features.py &&
-# python src/regression.py
+python src/connect_api.py &&
+python src/get_features.py &&
+python src/regression.py
 
 echo "git add"
 git add .
