7f8ecd3e0c31ccefa0541aff3808f712dbfda12a
Sivakorn Chong <sivakornchong@gmail.com>
2024-01-07 00:42:55 +0000

    Run from GCP VM

--- boot.sh
+++ boot.sh
@@ -1,9 +1,16 @@
 
-rm -r data/2024_pipe model
-mkdir data/2024_pipe
-mkdir model
+# rm -r data/2024_pipe model
+# mkdir data/2024_pipe
+# mkdir model
 
-# Run the Python script
-python src/connect_api.py &&
-python src/get_features.py &&
-python src/regression.py
+## Run the Python script
+# python src/connect_api.py &&
+# python src/get_features.py &&
+# python src/regression.py
+
+echo "git add"
+git add .
+echo "git commit"
+git commit -m "Run from GCP VM"
+echo "git push"
+git push
--- model/boot.sh
+++
@@ -1,9 +0,0 @@
-
-rm -r data/2024_pipe model
-mkdir data/2024_pipe
-mkdir model
-
-# Run the Python script
-python src/connect_api.py &&
-python src/get_features.py &&
-python src/regression.py
