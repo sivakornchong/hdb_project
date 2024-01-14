acd22a9192c060cb7aa1f2ba9b5676a3654452ef
Sivakorn Oak <sivakornchong@gmail.com>
2024-01-14 08:13:57 +0000

    add wrapper boot

---
+++ wrapper_boot.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+# Log file path
+LOG_FILE=~/log_file.txt
+
+# Function to log timestamps
+log_timestamp() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
+}
+
+# Log start time
+log_timestamp "Wrapper script started"
+
+# Run the first command
+log_timestamp "Executing boot.sh"
+~/hdb_project/boot.sh
+
+# Run the second command
+log_timestamp "Executing huggingface_boot.sh"
+~/HDB_resale_predict/huggingface_boot.sh
+
+# Log end time
+log_timestamp "Wrapper script completed"
+
+# Shut down the VM
+log_timestamp "Shutting down the VM"
+sudo shutdown -h now
+
