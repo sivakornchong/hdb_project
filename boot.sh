f38d83e1deffb4c9ff5b66fc7d1970479374022b
Sivakorn Oak <sivakornchong@gmail.com>
2024-01-14 07:48:48 +0000

    update boot

--- boot.sh
+++ boot.sh
@@ -8,7 +8,7 @@ conda env create -f environment.yaml --name HDB_pred || echo "Environment alread
 source /home/sivakornchong/miniconda3/etc/profile.d/conda.sh
 conda activate HDB_pred
 
-echo "Start running data extraction and model optimization"
+echo "Start running data extraction and model optimization" >> ~/log_file.txt
 rm -r data/2024_pipe model
 mkdir data/2024_pipe
 mkdir model
@@ -18,9 +18,13 @@ python src/connect_api.py &&
 python src/get_features.py &&
 python src/regression.py
 
+echo "Completed python scripts" >> ~/log_file.txt
+
 echo "git add"
 git add .
 echo "git commit"
 git commit -m "Run from GCP VM"
 echo "git push"
 git push
+
+echo "Pushed to github" >> ~/log_file.txt
