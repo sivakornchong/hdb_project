90e913fbbf71580c2b077c8c049c0037c59cb55e
oak <sivakornchong@gmail.com>
2025-06-14 16:28:11 -0700

    allow for skipping preprocessing

--- boot_dev.sh
+++ boot_dev.sh
@@ -4,4 +4,11 @@
 export AWS_ACCESS_KEY_ID=$(awk -F "=" '/aws_access_key_id/ {print $2}' ~/.aws/credentials | tr -d ' ')
 export AWS_SECRET_ACCESS_KEY=$(awk -F "=" '/aws_secret_access_key/ {print $2}' ~/.aws/credentials | tr -d ' ')
 
-python src/main.py
+
+if [ "$1" = "--skip-extraction" ]; then
+    python src/main.py --skip-extraction
+else
+    python src/main.py
+fi
+
+# To run skipping extraction: ./boot_dev --skip-extraction
